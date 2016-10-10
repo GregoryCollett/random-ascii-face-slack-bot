ab2e6171dd11a8f13e648ae7b627d89749c79fc5
Greg Collett <gregory.collett@live.co.uk>
2016-10-10 16:36:29 +0100

    minor updates + starting naming

--- src/ascii-face.js
+++ src/ascii-face.js
@@ -1,5 +1,10 @@
 var facesAsArray = require('./faces-as-array');
+var faceAsObject = require('./faces-as-object');
 
-module.exports = function randomAsciiFace(face) {
-  return facesAsArray[Math.floor(Math.random() * facesAsArray.length)];
+module.exports = function asciiFace(face) {
+  return (face) ?
+    (facesAsObject[face]) ?
+      facesAsObject[face] :
+      "Couldn't find that face, here a random one!     " + facesAsArray[Math.floor(Math.random() * facesAsArray.length)] :
+    facesAsArray[Math.floor(Math.random() * facesAsArray.length)];
 }
--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ module.exports = function() {
   return botBuidler(function(request) {
     if (request.text) {
       return {
-        text: asciiFace() + '  -=:=-  ' + request.text,
+        text: asciiFace(request.text),
         response_type: 'in_channel'
-      }
+      };
     }
     else {
       return {
         text: asciiFace(),
         response_type: 'in_channel'
-      }
+      };
     }
   });
 }
